1cb4527143312988a66e40a60283ab0901d4a98c
Max <max1220@users.noreply.github.com>
2015-11-21 13:36:41 +0000

    Added mount check, code cleanup
    Does not try to mount an already mounted badge anymore, and also code cleanup, comments, etc.

--- flash.lua
+++ flash.lua
@@ -1,10 +1,14 @@
 #!/usr/bin/env lua
+-- This script is designed to copy files to the 2015 CCC rad1o-badge in MSC mode, mount it before if it's not already, and unmount it & sync.
+
+-- Values in the table below are copied to the rad1o. Basicly "cp [value from table] [rad1o mountpoint]", so wildcards etc are allowed.
 local files = {
 	"l0dables/test.c1d",
 }
-
 local force = true -- Skipps questions
 
+
+-- Find dev by matching a pattern, including block ammount & size, in dmesg.
 local dmesg_p = io.popen("dmesg")
 if not dmesg_p then
 	print("Can't open dmesg, check your privileges!")
@@ -23,6 +27,7 @@ while true do
 	end
 end
 dmesg_p:close()
+
 
 if dev == nil then
 	print("No device found! Make sure it's in MSC mode!")
@@ -43,27 +48,54 @@ else
 	end
 end
 
+
+-- Check if we're already mounted
+local mounts_p = io.popen("mount")
+local mount = mount_p:read("*a"):match(dev .. " on (.-) type")
+if mount then
+	print("Already mounted!")
+end
+
+
 local mountpoint = "/mnt/tmp/" .. dev
-if not os.execute("mkdir -p " .. mountpoint) then
-	print("Can't create mount directory!")
-	os.exit(3)
+if mount then
+	-- We're already mounted
+	mountpoint = mount
+else
+	-- Not mounted yet, create mount point
+	if not os.execute("mkdir -p " .. mountpoint) then
+		print("Can't create mount directory!")
+		os.exit(3)
+	end
+
+	-- mount
+	if not os.execute("mount " .. dev .. " " .. mountpoint) then
+		print("Can't mount!")
+		os.exit(4)
+	end
 end
-if not os.execute("mount " .. dev .. " " .. mountpoint) then
-	print("Can't mount!")
-	os.exit(4)
-end
+
+
+-- Copy files
 for _, file in ipairs(files) do
 	if not os.execute("cp " .. file .. " " .. mountpoint) then
 		print("Can't cp!")
 		os.exit(5)
 	end
 end
+
+
+-- Unmount
 if not os.execute("umount " .. mountpoint) then
 	print("Can't unmount!")
 	os.exit(6)
 end
+
+
+-- Sync
 if not os.execute("sync") then
 	print("Can't sync!")
 	os.exit(7)
 end
+
 print("Ok! Device is ready to be unplugged!")
